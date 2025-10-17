63a1d400cab0b5cf5340ed219548b287801a4469
CassioDelfino <cassio.delfino@gft.com>
2025-10-17 15:55:48 -0300

    Atualização com banco de dados MySQL

--- DataBase.cs
+++ DataBase.cs
@@ -1,11 +1,32 @@
 using MySql.Data.MySqlClient;
 
-public class Database
+namespace DesafioPOO.Models
 {
-    private string connectionString = "Server=localhost;Database=corretora;User=root;Password=senha;";
-
-    public MySqlConnection GetConnection()
+    public class DataBase
     {
-        return new MySqlConnection(connectionString);
+        private string connectionString = "server=localhost;user=root;password=root;database=corretora";
+
+        public void SalvarImovel(Imovel imovel)
+        {
+            using (var conexao = new MySqlConnection(connectionString))
+            {
+                conexao.Open();
+                string query = "INSERT INTO imoveis (tipo, endereco, numero, alugado, nome_proprietario, telefone, cpf) " +
+                               "VALUES (@tipo, @endereco, @numero, @alugado, @nome, @telefone, @cpf)";
+
+                using (var cmd = new MySqlCommand(query, conexao))
+                {
+                    cmd.Parameters.AddWithValue("@tipo", imovel is Casa ? "casa" : "apartamento");
+                    cmd.Parameters.AddWithValue("@endereco", imovel.GetEndereco());
+                    cmd.Parameters.AddWithValue("@numero", imovel.GetNumero());
+                    cmd.Parameters.AddWithValue("@alugado", imovel.GetAlugado());
+                    cmd.Parameters.AddWithValue("@nome", imovel.Proprietario.Nome);
+                    cmd.Parameters.AddWithValue("@telefone", imovel.Proprietario.Telefone);
+                    cmd.Parameters.AddWithValue("@cpf", imovel.Proprietario.CPF);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
